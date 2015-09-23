d96515ba530e30bcb93bd99ade9dc7bf08684baa
Joe Barnes <barnesjd@gmail.com>
2015-09-23 13:41:56 -0500

    Adding a clarifying comment to the Java8 evens

--- src/main/java/com/joescii/EvensJava8.java
+++ src/main/java/com/joescii/EvensJava8.java
@@ -3,6 +3,7 @@ package com.joescii;
 import java.util.stream.IntStream;
 
 public class EvensJava8 {
+    // Notice that n and i are never _reassigned_
     public static IntStream evensFirst(int n) {
         return IntStream.rangeClosed(1, n)
                 .map((i) -> i * 2);
