343a90b15785067c47064afff32b10190966ef02
Joe Barnes <barnesjd@gmail.com>
2015-09-27 14:52:09 -0500

    Correcting the name of a variable

--- src/main/java/com/joescii/EvensJava7.java
+++ src/main/java/com/joescii/EvensJava7.java
@@ -4,9 +4,9 @@ import java.util.*;
 
 public class EvensJava7 {
     public static List<Integer> firstNEvens(int n) {
-        List<Integer> squares = new ArrayList<Integer>();
+        List<Integer> evens = new ArrayList<Integer>();
         for(int i=1; i<=n; i++)
-            squares.add(i*2);
-        return squares;
+            evens.add(i*2);
+        return evens;
     }
 }
